e2c664f99d26158f30ed1d1de4865d5190bdb6f6
Pantantey <102394928+Pantantey@users.noreply.github.com>
2024-11-06 00:09:25 -0600

    button

---
+++ components/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+  onPress: () => void;
+}
+
+const CustomButton = ({ title, onPress}: ButtonProps) => {
+  return (
+    <TouchableOpacity style={[styles.button]} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+//button styles
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#4C51F7',
+    borderRadius: 24,
+    width: '100%',
+    height: 48,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontFamily: 'RedHatDisplay_700Bold',
+    textAlign: 'center',
+  },
+});
+
+export default CustomButton;
